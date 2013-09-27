b0bdfd1979c823d81e4b2d60ba0b2c44fdeef78a
dscholz <dscholz@student.tgm.ac.at>
2013-09-27 13:47:23 +0200

    Fabrik hinzugefügt

---
+++ src/tgm/sew/hit/roboterfabrik/Fabrik.java
@@ -0,0 +1,11 @@
+package tgm.sew.hit.roboterfabrik;
+
+/**
+ * Erstellt die Threads und stellt die Verbindung zwischen ihnen dar
+ * 
+ * @author Dominik
+ * @version 0.1
+ */
+public class Fabrik {
+    
+}
