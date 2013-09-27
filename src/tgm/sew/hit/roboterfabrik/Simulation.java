a9e769c35b8b66416edacb98221073a9576da505
dscholz <dscholz@student.tgm.ac.at>
2013-09-27 12:20:47 +0200

    Initial Commit

---
+++ src/tgm/sew/hit/roboterfabrik/Simulation.java
@@ -0,0 +1,19 @@
+package tgm.sew.hit.roboterfabrik;
+
+/**
+ * Die Main Klasse der Roboterfabrik
+ * 
+ * @author Dominik
+ * @version 0.1
+ */
+public class Simulation {
+
+    /**
+     * Wird beim starten aufgerufen
+     * 
+     * @param args die Command-line Argumente
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
