c898d24410e90e180a55726e8662b676585d4a3a
dscholz <dscholz@student.tgm.ac.at>
2013-10-02 08:55:14 +0200

    Teil auslese Methode geadded

--- src/tgm/sew/hit/roboterfabrik/LagerMitarbeiter.java
+++ src/tgm/sew/hit/roboterfabrik/LagerMitarbeiter.java
@@ -1,5 +1,8 @@
 package tgm.sew.hit.roboterfabrik;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Stellt einen Lager Mitarbeiter dar,
  * welcher sich um die Lagerung der Teile kümmert
@@ -8,5 +11,22 @@ package tgm.sew.hit.roboterfabrik;
  * @version 0.1
  */
 public class LagerMitarbeiter {
+    
+    /**
+     * Konstrutor des LagerMitarbeiters
+     */
+    public LagerMitarbeiter() {
+        
+    }
+    
+    /**
+     * Liest ein Teil ein
+     * @param teilType welche Art von Teil geladen werden soll
+     * @return ein Teil des entsprechenden TeilTypes aus dem Lager
+     */
+    public Teil leseTeil(TeilType teilType) {
+        List<Integer> li = new ArrayList<>();
+        return new Teil(teilType,li);
+    }
     
 }
